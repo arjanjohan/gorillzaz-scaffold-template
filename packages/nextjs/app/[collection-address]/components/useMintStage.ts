b6adc7f06901785936ca982ffb15ecdc1f9edad2
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-01-12 21:56:18 +0100

    Fix display when api calls fail

--- packages/nextjs/app/[collection-address]/components/useMintStage.ts
+++ packages/nextjs/app/[collection-address]/components/useMintStage.ts
@@ -6,44 +6,52 @@ export function useMintStage(
   stageName: string,
   accountAddress: `0x${string}` | undefined,
 ) {
-  console.log("COLLECTION ADDRESS", collectionAddress);
-  console.log("STAGE NAME", stageName);
-  console.log("ACCOUNT ADDRESS", accountAddress);
-
   const launchpad = useLaunchpad();
 
   return useQuery({
     queryKey: ["mintStage", collectionAddress, stageName, accountAddress],
     queryFn: async () => {
-      const isAllowlisted = accountAddress
-        ? launchpad.view.is_allowlisted({
-            functionArguments: [collectionAddress, stageName, accountAddress],
-            typeArguments: [],
-          })
-        : undefined;
+      let isAllowlisted = false;
+      if (accountAddress) {
+        try {
+          isAllowlisted = (
+            await launchpad.view.is_allowlisted({
+              functionArguments: [collectionAddress, stageName, accountAddress],
+              typeArguments: [],
+            })
+          )[0];
+        } catch (error) {}
+      }
 
-      const mintFee = launchpad.view.get_mint_fee({
-        functionArguments: [collectionAddress, stageName, 1],
-        typeArguments: [],
-      });
+      const mintFee = (
+        await launchpad.view.get_mint_fee({
+          functionArguments: [collectionAddress, stageName, 1],
+          typeArguments: [],
+        })
+      )[0];
 
       const stageTimes = launchpad.view.get_mint_stage_start_and_end_time({
         functionArguments: [collectionAddress, stageName],
         typeArguments: [],
       });
 
-      const mintBalance = accountAddress
-        ? launchpad.view.get_mint_balance({
-            functionArguments: [collectionAddress, stageName, accountAddress],
-            typeArguments: [],
-          })
-        : undefined;
+      let mintBalance = "0";
+      if (accountAddress) {
+        try {
+          mintBalance = (
+            await launchpad.view.get_mint_balance({
+              functionArguments: [collectionAddress, stageName, accountAddress],
+              typeArguments: [],
+            })
+          )[0];
+        } catch (error) {}
+      }
 
       return {
-        isAllowlisted: (await isAllowlisted)?.[0],
-        mintFee: (await mintFee)?.[0],
+        isAllowlisted,
+        mintFee,
         stageTimes: await stageTimes,
-        mintBalance: parseInt((await mintBalance)?.[0] ?? "0"),
+        mintBalance: parseInt(mintBalance),
       };
     },
   });
